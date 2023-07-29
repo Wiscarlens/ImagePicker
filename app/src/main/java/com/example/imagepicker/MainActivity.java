d304a8974add0c71cedc4c4a144ddf6f71cc48a3
Wiscarlens <wiscarlens@gmail.com>
2023-07-29 15:33:04 -0400

    clean code

--- app/src/main/java/com/example/imagepicker/MainActivity.java
+++ app/src/main/java/com/example/imagepicker/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.imagepicker;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
     ImageView imageView;
-    int SELECT_IMAGE_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,26 +24,20 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityResultLauncher<Intent> imagePickerLauncher =
                 registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-                        new ActivityResultCallback<ActivityResult>() {
-                            @Override
-                            public void onActivityResult(ActivityResult result) {
-                                if (result.getResultCode() == RESULT_OK) {
-                                    // Copy Image
-                                    assert result.getData() != null;
-                                    Uri uri = result.getData().getData();
-                                    imageView.setImageURI(uri);
-                                }
+                        result -> {
+                            if (result.getResultCode() == RESULT_OK) {
+                                // Copy Image
+                                assert result.getData() != null;
+                                Uri uri = result.getData().getData();
+                                imageView.setImageURI(uri);
                             }
                         });
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                imagePickerLauncher.launch(intent);
-            }
+        btn.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            imagePickerLauncher.launch(intent);
         });
 
     }
